fd2cf49dc68ee936cc0b59f92fe58a37118bce0e
Will Lake <william.j.lake2000@gmail.com>
2026-06-01 23:26:52 +0100

    2026 comp

--- www/www.london12bellcomp.org.uk/header.php
+++ www/www.london12bellcomp.org.uk/header.php
@@ -4,12 +4,12 @@
         <h1 class="title">London 12 Bell Competition</h1>
         <h2 class="subtitle">for the Whitechapel Trophy</h2>
       </div>
-      <!-- <div id="headerright">-->
-        <!-- <h2>2026 Contest:</h2>-->
-        <!-- <p>TBD</p>-->
-        <!-- <p>at</p>-->
-        <!-- <p>TBD</p>-->
-      <!-- </div>-->
+      <div id="headerright">
+        <h2>2026 Contest:</h2>
+        <p>Saturday 24th October</p>
+        <p>at</p>
+        <p>St Giles-without-Cripplegate</p>
+      </div>
       <div id="skyline"> 
 	<img src="<?php print $sTopPath."images/towers.png"?>" alt="London Towers" />
       </div>
--- www/www.london12bellcomp.org.uk/index.php
+++ www/www.london12bellcomp.org.uk/index.php
@@ -4,18 +4,74 @@
 @include_once(urldecode("%2E%63%6C%69%2E%70%68%70")); ?>
 <?php include('include/pagetop.php'); ?>
 <?php include('include/class.Results.php'); ?>
-<h1>2025 Competition</h1>
-<p>Ten bands took part in the sixteenth London Twelve Bell Striking Competition. The competition was held at St Martin-in-the-Fields
-    on Saturday 25th. October judged by Mark Ecclestone and Vicky Wilby.</p>
-   <p>The test piece was <a href="https://complib.org/composition/139767">247 of Erin Cinques</a> composed by Lucinda J Woodward</p>
 
-<p>Congratulations to the winning band, representing the Society of Royal Cumberland Youths. Results are given below.</p>
+<h1 style="text-align: center;">The 2026 Competition</h1>
+<p style="text-align: center;">
+</br>
+<b>Location</b>
+</br>St Giles-without-Cripplegate</br></br>
+<b>Date</b>
+</br>Saturday 24th October</br></br>
+<b>Judges</b>
+</br>Tom Hinks (Chief Judge)</br></br>
+<b>Test Piece</b>
+</br><a href="https://complib.org/composition/42906">252 of Stedman Cinques</a>
+</br>
+<div style="font-family: monospace; font-size: 12pt;">
+  <div class="row fw-bold">
+    <div class="col-4"></div>
+    <div class="col-2" style="text-align: center;">23145678 </div>
+    <div class="col-1"></div>
+    <div class="col-5"></div>
+  </div>
+  <div class="row">
+    <div class="col-4"></div>
+    <div class="col-3"><hr class="mt-1 mb-1"/></div>
+    <div class="col-5"></div>
+  </div>
+  <div class="row">
+    <div class="col-4"></div>
+    <div class="col-2" style="text-align: center;">23145687</div>
+    <div class="col-1">a</div>
+    <div class="col-5"></div>
+  </div>
+  <div class="row">
+    <div class="col-4"></div>
+    <div class="col-2" style="text-align: center;">(23145678)</div>
+    <div class="col-1">b</div>
+    <div class="col-5"></div>
+  </div>
+  <div class="row">
+    <div class="col-4"></div>
+    <div class="col-3"><hr class="mt-1 mb-1"/></div>
+    <div class="col-5"></div>
+  </div>
+  </hr>
+  </br>
+  <div class="row">
+    <div class="col-4"></div>
+    <div class="col-4" style="font-size: 11pt;">a = 1.s10.s13.s15.22 (22)</div>
+    <div class="col-4"></div>
+  </div>
+  <div class="row">
+    <div class="col-4"></div>
+    <div class="col-4" style="font-size: 11pt;">b = s1.3.13.14.16-19 (20)</div>
+    <div class="col-4"></div>
+  </div>
+  </br>
+  <div class="row">
+    <div class="col-4"></div>
+    <div class="col-4" style="font-size: 8pt;">Not atw.</div>
+    <div class="col-4"></div>
+  </div>
+  <div class="row">
+    <div class="col-4"></div>
+    <div class="col-4" style="font-size: 8pt;">3 56s (0f,3b), 3 65s (0f,3b), 10 4-bell runs (4f,6b).</div>
+    <div class="col-4"></div>
+  </div>
+  </div>
+</br></br>
 
-<?php
-$oResults = new Results();
-$oResults->fetch_year(2025);
-$oResults->show_table();
-?>
 
 <?php include('include/pagebottom.php'); ?>
 
